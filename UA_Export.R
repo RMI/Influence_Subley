39be80fa2ce245bde87afd1aa85fd3960354237b
glnnhoffman <ghoffman@rmi.org>
2024-08-28 11:54:35 -0600

    Add UA export and updates

--- UA_Export.R
+++ UA_Export.R
@@ -1,52 +1,100 @@
 
 setwd("C:/Users/ghoffman/OneDrive - RMI/01. Projects/Consultation/Influence_Subley")
+
+setwd("C:/Users/ghoffman/OneDrive - RMI/01. Projects/Consultation/UnivAnalytics_Export")
 ### get packages and functions
 source("packages.R")  
 source("functions.R")  
 
 load_dot_env('Renviron.env')
 
-### get referral sites
-referralSites <- read.xlsx('Referral Site Categories.xlsx', sheet = 'All Referral Sites')
+# load UA metrics and dims of interest
+target_meta <- read.xlsx('metadataUA_keepers.xlsx')
 
-# PEM cert file location
-cert <- normalizePath("C:\\Users\\ghoffman\\OneDrive - RMI\\01. Projects\\DigiCertGlobalRootCA.crt.pem")
+dims <- target_meta %>%
+  filter(type == 'DIMENSION')
 
-# get most recent date_added from MySQL
+metrics <- target_meta %>%
+  filter(type == 'METRIC')
+
+
 con <- dbConnect(
   RMariaDB::MariaDB(),
-  dbname = 'rmi_GA4',
+  dbname = 'rmi_UA',
   username = Sys.getenv('DBASE_USER'),
   password = Sys.getenv("DBASE_PWD"),
   host = Sys.getenv('DBASE_IP'),
   port = '3306',
-  ssl.ca = cert
+  ssl.ca = normalizePath("C:\\Users\\ghoffman\\OneDrive - RMI\\01. Projects\\DigiCertGlobalRootCA.crt.pem")
 )
 
-query <- "SELECT max(date_added) FROM traffic_all"
+#query <- "SELECT max(date_added) FROM traffic_all"
+#mostRecent <- dbGetQuery(con, query)
 
-mostRecent <- dbGetQuery(con, query)
+
 currentDate <- paste(Sys.Date())
-oneYearAgo <- ymd(currentDate) - years(1)
-
-# Data range set as day before today and most recent date in database
 dayBefore <- paste(as.Date(currentDate) - days(1))
+#weekBefore <- paste(as.Date(currentDate) - days(7))
 weekBefore <- paste(as.Date(mostRecent$`max(date_added)`))
-dateRangeGA <- c(paste(weekBefore), paste(dayBefore))
-
+oneYearAgo <- ymd(currentDate) - years(1)
 
 
 ## GA Authentication
+
+ga_auth()
 ga_auth(email = "ghoffman@rmi.org")
 
+ga_accounts()
+
+
 #' set GA variables and property ID
+dateRangeGA <- c(paste(weekBefore), paste(dayBefore))
+
+
+rmiPropertyID <- 2676570
+
+UA-2676570-1
+
+
+metadataUA <- ga_meta(version = "universal", rmiPropertyID)
+
 rmiPropertyID <- 354053620
 metadataGA4 <- ga_meta(version = "data", rmiPropertyID)
 
-## set export
-ss <- 'ParsleyReplaceDataset.xlsx'
+
+#' SUMMARY
+#' 1. Pulls list of pages from campaign key file
+#' 2. Gets page title and metadata by webscraping provided URLs
+#' 3. For all pages
+#'      Get key metrics - page views, users, engagement duration
+#'      Get acquisition data - sessions + conversions - broken down by channel
+#'      Get social media acquisition data - sessions
+#'      Get page views broken down by country and region
+#'      Get page traffic - sessions - driven by referral sources that have been identified as “Media” 
+#'        - These sources are defined in the referralSites file
 
 
+
+# Sitewide metrics
+
+# □ Traffic source (acquisition)
+# ® Take a look at the referral categories
+# ® May want to parse out more granular to differentiate different news outlets vs. other RMI pages vs. social traffic
+# □ Geography
+# ® Country level (need)
+# ® State level (nice to have)
+# □ Engaged page views
+# ® Number
+# ® Rate
+# □ Time spent on page
+# □ Users
+# ® Returning
+# ® New
+# □ Conversions
+# ® Report downloads
+# ® Online donations (via GA4 Goal)
+# ® Event reg (Goal)
+# Email sign up (Goal)
 
 
 # Get all pages on rmi.org to use as reference table.
@@ -64,63 +112,11 @@ pages_filter <- pages %>%
   filter(pageTitle != 'Page not found - RMI') %>%
   filter(pageTitle != '(not set)') %>%
   filter(pageTitle != '') %>%
-  filter(!grepl('rmi.org/people', fullPageUrl)) %>% 
+  filter(!grepl('rmi.org/people', fullPageUrl)) %>%
   filter(screenPageViews > 99) %>%
   mutate(pageURL = paste('http://', fullPageUrl, sep = ''), pageTitle = gsub(' - RMI', '', pageTitle),
          site = 'rmi.org', metadata = '', pageType = '')
 
-
-############# Working section for testing/exploration
-
-# test_date <- ga_data(
-#   rmiPropertyID,
-#   metrics = c('screenPageViews', "totalUsers", "userEngagementDuration", 'newUsers'),
-#  # dimensions = c("pageTitle", "country","city","region", 'sessionSource'),
-#   dimensions = c("pageTitle", "country", "yearMonth"),
-#   date_range = dateRangeGA,
-#   dim_filters = ga_data_filter("pageTitle" == pages),
-#   limit = -1)
-# 
-# test_date %>%
-#   group_by(yearMonth) %>%
-#   summarise(views = sum(screenPageViews))
-# 
-# # df for testing, top 5 rows of pages
-# test_page <- pages_filter %>%
-#   head()
-# 
-# url <- as.character(test_page[3, 'pageURL'])
-# 
-# url_tb <- url %>%
-#   read_html() %>% 
-#   html_nodes('script') %>% 
-#   html_text() %>% 
-#   as.data.frame()
-#   
-# pageData <- getPageData(test_page)
-# 
-# for(i in 1:nrow(test_page)){
-#   url <- as.character(test_page[i, 'pageURL'])
-#   
-# url_tb <- url %>%
-#   read_html() %>% 
-#   html_nodes('script') %>% 
-#   html_text() %>% 
-#   as.data.frame() %>% 
-#   rename(node = 1) %>% 
-#   filter(grepl('schema.org', node)) %>% 
-#   mutate(keywords = sub('.*keywords\\"\\:\\[', "", node),
-#          keywords = gsub('\\].*', "", keywords))
-# }
-# 
-# campaignPages <- ga_data(
-#   rmiPropertyID,
-#   metrics = c('screenPageViews', "totalUsers", "userEngagementDuration"),
-#   dimensions = c("pageTitle", "date"),
-#   date_range = dateRangeGA,
-#   dim_filters = ga_data_filter("pageTitle" == pages),
-#   limit = -1
-# )
 
 ###################################################333
 ########################################################33
@@ -151,30 +147,31 @@ mediaReferrals <- getReferrals(rmiPropertyID, pages)
 pageMetrics <- pageMetrics %>%
   filter(!is.na(date)) %>%
   mutate(uid = paste(pageTitle, date, sep = "_"))
-  
-  
-  #' bind page metrics and pivot table so that sessions/conversions are stored in one column
-  #' this is to make a Power BI table column that changes based on an applied filter
+
+
+#' bind page metrics and pivot table so that sessions/conversions are stored in one column
+#' this is to make a Power BI table column that changes based on an applied filter
 allTraffic <- pageData %>% 
-    select(site, pageTitle) %>% 
-    distinct() %>% 
-    plyr::rbind.fill(acquisition) %>% 
-    pivot_longer(cols = c(Sessions:'Form Submissions'), names_to = "type", values_to = "count") %>% 
-    filter(!is.na(count), count > 0, !is.na(date)) %>%
-    mutate(count = round(count, 1), uid = paste(pageTitle, date, sep = "_")) %>%
-    # left_join(select(pageMetrics, c(pageTitle, screenPageViews:avgEngagementDuration, pageType, icon)), by = c('pageTitle','date')) %>% 
-    left_join(select(pageMetrics, c(uid, screenPageViews:avgEngagementDuration, pageType, icon)), by = c('uid')) %>% 
-     mutate(count = as.numeric(ifelse(is.na(count), 0, count))) %>% 
-    filter(defaultChannelGroup != 'Unassigned' & !is.na(defaultChannelGroup))
-  
+  select(site, pageTitle) %>% 
+  distinct() %>% 
+  plyr::rbind.fill(acquisition) %>% 
+  pivot_longer(cols = c(Sessions:'Form Submissions'), names_to = "type", values_to = "count") %>% 
+  filter(!is.na(count), count > 0, !is.na(date)) %>%
+  mutate(count = round(count, 1), uid = paste(pageTitle, date, sep = "_")) %>%
+  # left_join(select(pageMetrics, c(pageTitle, screenPageViews:avgEngagementDuration, pageType, icon)), by = c('pageTitle','date')) %>% 
+  left_join(select(pageMetrics, c(uid, screenPageViews:avgEngagementDuration, pageType, icon)), by = c('uid')) %>% 
+  mutate(count = as.numeric(ifelse(is.na(count), 0, count))) %>% 
+  filter(defaultChannelGroup != 'Unassigned' & !is.na(defaultChannelGroup))
+
 numChannels <- allTraffic %>% group_by(uid, type) %>% summarize(numChannels = n())
-  
+
 allTraffic <- allTraffic %>% 
-    left_join(numChannels) %>% 
-    mutate('page_views' = round(screenPageViews/numChannels, 2)) %>% 
-    select(-c(screenPageViews, numChannels))
+  left_join(numChannels) %>% 
+  mutate('page_views' = round(screenPageViews/numChannels, 2)) %>% 
+  select(-c(screenPageViews, numChannels))
 
 #########################################################
+
 
 # page titles and IDs from database
 query <- dbSendQuery(con, "SELECT Id, pageTitle FROM rmi_pages")
@@ -186,12 +183,11 @@ rmi_pages <- data %>%
   mutate(pageTitle = gsub(' - RMI', '', pageTitle)) %>%
   left_join(db_pages, by = c('pageTitle')) %>%
   filter(is.na(Id))
-  
+
 # import new pages to database, if new pages are present
 if(length(rmi_pages >1)){
   dbWriteTable(con, 'rmi_pages', rmi_pages, append = T)
 }
-
 
 # Get updated complete list of page titles
 query <- dbSendQuery(con, "SELECT Id, pageTitle FROM rmi_pages")
@@ -239,7 +235,7 @@ con <- dbConnect(
   password = Sys.getenv("DBASE_PWD"),
   host = Sys.getenv('DBASE_IP'),
   port = '3306',
-  ssl.ca = cert
+  ssl.ca = normalizePath("C:\\Users\\ghoffman\\OneDrive - RMI\\01. Projects\\DigiCertGlobalRootCA.crt.pem")
 )
 
 
